f21593c91283c56697d81ff6c14919dfc7943108
JiphuTzu <jiphutzu@gmail.com>
2025-12-04 14:17:48 +0800

    - Injector路径修改

---
+++ Runtime/SimplifyIoC/Injectors/InjectionBinder.cs
@@ -0,0 +1,301 @@
+/*
+ * Copyright 2013 ThirdMotion, Inc.
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *		Unless required by applicable law or agreed to in writing, software
+ *		distributed under the License is distributed on an "AS IS" BASIS,
+ *		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *		See the License for the specific language governing permissions and
+ *		limitations under the License.
+ */
+
+/*
+ * @class SimplifyIoC.Injectors.InjectionBinder
+ * 
+ * The Binder for creating Injection mappings.
+ * 
+ * @see SimplifyIoC.Injectors.IInjectionBinder
+ * @see SimplifyIoC.Injectors.IInjectionBinding
+ */
+
+using System;
+using System.Collections.Generic;
+using SimplifyIoC.Reflectors;
+using SimplifyIoC.Framework;
+
+namespace SimplifyIoC.Injectors
+{
+    public class InjectionBinder : Binder, IInjectionBinder
+    {
+        private Injector _injector;
+        protected Dictionary<Type, Dictionary<Type, IInjectionBinding>> suppliers = new Dictionary<Type, Dictionary<Type, IInjectionBinding>>();
+
+        public InjectionBinder()
+        {
+            injector = new Injector();
+            injector.binder = this;
+            injector.reflector = new ReflectionBinder();
+        }
+
+        public object GetInstance(Type key)
+        {
+            return GetInstance(key, null);
+        }
+
+        public virtual object GetInstance(Type key, object name)
+        {
+            var binding = GetBinding(key, name);
+            if (binding == null)
+            {
+                throw new InjectionException("InjectionBinder has no binding for:\n\tkey: " + key + "\nname: " + name, InjectionExceptionType.NULL_BINDING);
+            }
+            var instance = GetInjectorForBinding(binding).Instantiate(binding, false);
+            injector.TryInject(binding, instance);
+
+            return instance;
+        }
+
+        protected virtual Injector GetInjectorForBinding(IInjectionBinding binding)
+        {
+            return injector;
+        }
+
+        public T GetInstance<T>()
+        {
+            var instance = GetInstance(typeof(T));
+            var retv = (T)instance;
+            return retv;
+        }
+
+        public T GetInstance<T>(object name)
+        {
+            var instance = GetInstance(typeof(T), name);
+            var retv = (T)instance;
+            return retv;
+        }
+
+        public override IBinding GetRawBinding()
+        {
+            return new InjectionBinding(Resolver);
+        }
+
+        public Injector injector
+        {
+            get
+            {
+                return _injector;
+            }
+            set
+            {
+                if (_injector != null)
+                {
+                    _injector.binder = null;
+                }
+                _injector = value;
+                _injector.binder = this;
+            }
+        }
+
+        public new IInjectionBinding Bind<T>()
+        {
+            return base.Bind<T>() as IInjectionBinding;
+        }
+
+        public IInjectionBinding Bind(Type key)
+        {
+            return base.Bind(key) as IInjectionBinding;
+        }
+
+        public new virtual IInjectionBinding GetBinding<T>()
+        {
+            return base.GetBinding<T>() as IInjectionBinding;
+        }
+
+        public new virtual IInjectionBinding GetBinding<T>(object name)
+        {
+            return base.GetBinding<T>(name) as IInjectionBinding;
+        }
+
+        public new virtual IInjectionBinding GetBinding(object key)
+        {
+            return base.GetBinding(key) as IInjectionBinding;
+        }
+
+        public new virtual IInjectionBinding GetBinding(object key, object name)
+        {
+            return base.GetBinding(key, name) as IInjectionBinding;
+        }
+
+        public int ReflectAll()
+        {
+            var list = new List<Type>();
+            foreach (var pair in bindings)
+            {
+                var dict = pair.Value;
+                foreach (var bPair in dict)
+                {
+                    var binding = bPair.Value;
+                    var t = (binding.value is Type) ? (Type)binding.value : binding.value.GetType();
+                    if (list.IndexOf(t) == -1)
+                    {
+                        list.Add(t);
+                    }
+                }
+            }
+            return Reflect(list);
+        }
+
+        public int Reflect(List<Type> list)
+        {
+            var count = 0;
+            foreach (var t in list)
+            {
+                //Reflector won't permit primitive types, so screen them
+                if (t.IsPrimitive || t == typeof(Decimal) || t == typeof(string))
+                {
+                    continue;
+                }
+                count++;
+                injector.reflector.Get(t);
+            }
+            return count;
+        }
+
+        // protected override IBinding PerformKeyValueBindings(List<object> keyList, List<object> valueList)
+        // {
+        //     IBinding binding = null;
+        //
+        //     // Bind in order
+        //     foreach (var key in keyList)
+        //     {
+        //         var keyType = Type.GetType(key as string);
+        //         if (keyType == null)
+        //         {
+        //             throw new BinderException("A runtime Injection Binding has resolved to null. Did you forget to register its fully-qualified name?\n Key:" + key, BinderExceptionType.RUNTIME_NULL_VALUE);
+        //         }
+        //         if (binding == null)
+        //         {
+        //             binding = Bind(keyType);
+        //         }
+        //         else
+        //         {
+        //             binding = binding.Bind(keyType);
+        //         }
+        //     }
+        //     foreach (var value in valueList)
+        //     {
+        //         var valueType = Type.GetType(value as string);
+        //         if (valueType == null)
+        //         {
+        //             throw new BinderException("A runtime Injection Binding has resolved to null. Did you forget to register its fully-qualified name?\n Value:" + value, BinderExceptionType.RUNTIME_NULL_VALUE);
+        //         }
+        //         binding = binding.To(valueType);
+        //     }
+        //
+        //     return binding;
+        // }
+
+        /// Additional options: ToSingleton, CrossContext
+        // override protected IBinding AddRuntimeOptions(IBinding b, List<object> options)
+        // {
+        //     base.AddRuntimeOptions(b, options);
+        //     IInjectionBinding binding = b as IInjectionBinding;
+        //     if (options.IndexOf("ToSingleton") > -1)
+        //     {
+        //         binding.ToSingleton();
+        //     }
+        //     if (options.IndexOf("CrossContext") > -1)
+        //     {
+        //         binding.CrossContext();
+        //     }
+        //     IEnumerable<Dictionary<string, object>> dict = options.OfType<Dictionary<string, object>>();
+        //     if (dict.Any())
+        //     {
+        //         Dictionary<string, object> supplyToDict = dict.First(a => a.Keys.Contains("SupplyTo"));
+        //         if (supplyToDict != null)
+        //         {
+        //             foreach (KeyValuePair<string, object> kv in supplyToDict)
+        //             {
+        //                 if (kv.Value is string)
+        //                 {
+        //                     Type valueType = Type.GetType(kv.Value as string);
+        //                     binding.SupplyTo(valueType);
+        //                 }
+        //                 else
+        //                 {
+        //                     List<object> values = kv.Value as List<object>;
+        //                     for (int a = 0, aa = values.Count; a < aa; a++)
+        //                     {
+        //                         Type valueType = Type.GetType(values[a] as string);
+        //                         binding.SupplyTo(valueType);
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+
+        //     return binding;
+        // }
+
+        public IInjectionBinding GetSupplier(Type injectionType, Type targetType)
+        {
+            if (suppliers.ContainsKey(targetType))
+            {
+                if (suppliers[targetType].ContainsKey(injectionType))
+                {
+                    return suppliers[targetType][injectionType];
+                }
+            }
+            return null;
+        }
+
+        public void Unsupply(Type injectionType, Type targetType)
+        {
+            var binding = GetSupplier(injectionType, targetType);
+            if (binding != null)
+            {
+                suppliers[targetType].Remove(injectionType);
+                binding.Unsupply(targetType);
+            }
+        }
+
+        public void Unsupply<T, U>()
+        {
+            Unsupply(typeof(T), typeof(U));
+        }
+
+        protected override void Resolver(IBinding binding)
+        {
+            var iBinding = binding as IInjectionBinding;
+            var supply = iBinding.GetSupply();
+
+            if (supply != null)
+            {
+                foreach (var a in supply)
+                {
+                    var aType = a as Type;
+                    if (suppliers.ContainsKey(aType) == false)
+                    {
+                        suppliers[aType] = new Dictionary<Type, IInjectionBinding>();
+                    }
+                    var keys = iBinding.key as object[];
+                    foreach (var key in keys)
+                    {
+                        var keyType = key as Type;
+                        if (!suppliers[aType].ContainsKey(keyType))
+                        {
+                            suppliers[aType][keyType] = iBinding;
+                        }
+                    }
+                }
+            }
+
+            base.Resolver(binding);
+        }
+    }
+}
---
+++ Runtime/SimplifyIoC/Injectors/InjectionBinding.cs
@@ -0,0 +1,210 @@
+/*
+ * Copyright 2013 ThirdMotion, Inc.
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *		Unless required by applicable law or agreed to in writing, software
+ *		distributed under the License is distributed on an "AS IS" BASIS,
+ *		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *		See the License for the specific language governing permissions and
+ *		limitations under the License.
+ */
+
+/**
+ * @class SimplifyIoC.Injectors.InjectionBinding
+ * 
+ * The Binding for Injections.
+ * 
+ * @see SimplifyIoC.Injectors.IInjectionBinding
+ */
+
+using System;
+using SimplifyIoC.Framework;
+
+namespace SimplifyIoC.Injectors
+{
+    public class InjectionBinding : Binding, IInjectionBinding
+    {
+
+        private ISemiBinding supplyList = new SemiBinding();
+
+        public InjectionBinding(Binder.BindingResolver resolver)
+        {
+            this.resolver = resolver;
+            keyConstraint = BindingConstraintType.MANY;
+            valueConstraint = BindingConstraintType.ONE;
+            supplyList.constraint = BindingConstraintType.MANY;
+        }
+
+        public InjectionBindingType type { get; set; } = InjectionBindingType.DEFAULT;
+
+        public bool toInject { get; private set; } = true;
+
+        public IInjectionBinding ToInject(bool value)
+        {
+            toInject = value;
+            return this;
+        }
+
+        public bool isCrossContext { get; private set; } = false;
+
+        public IInjectionBinding ToSingleton()
+        {
+            //If already a value, this mapping is redundant
+            if (type == InjectionBindingType.VALUE)
+                return this;
+
+            type = InjectionBindingType.SINGLETON;
+            if (resolver != null)
+            {
+                resolver(this);
+            }
+            return this;
+        }
+
+        public IInjectionBinding ToValue(object o)
+        {
+            type = InjectionBindingType.VALUE;
+            SetValue(o);
+            return this;
+        }
+
+        public IInjectionBinding SetValue(object o)
+        {
+
+            var objType = o.GetType();
+
+            var keys = key as object[];
+            var aa = keys.Length;
+            //Check that value is legal for the provided keys
+            for (var a = 0; a < aa; a++)
+            {
+                var aKey = keys[a];
+                var keyType = (aKey is Type) ? aKey as Type : aKey.GetType();
+                if (keyType.IsAssignableFrom(objType) == false && (HasGenericAssignableFrom(keyType, objType) == false))
+                {
+                    throw new InjectionException("Injection cannot bind a value that does not extend or implement the binding type.", InjectionExceptionType.ILLEGAL_BINDING_VALUE);
+                }
+            }
+            To(o);
+            return this;
+        }
+
+        protected bool HasGenericAssignableFrom(Type keyType, Type objType)
+        {
+            //FIXME: We need to figure out how to determine generic assignability
+            if (keyType.IsGenericType == false)
+                return false;
+
+            return true;
+        }
+
+        protected bool IsGenericTypeAssignable(Type givenType, Type genericType)
+        {
+            var interfaceTypes = givenType.GetInterfaces();
+
+            foreach (var it in interfaceTypes)
+            {
+                if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
+                    return true;
+            }
+
+            if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+                return true;
+
+            var baseType = givenType.BaseType;
+            if (baseType == null) return false;
+
+            return IsGenericTypeAssignable(baseType, genericType);
+        }
+
+        public IInjectionBinding CrossContext()
+        {
+            isCrossContext = true;
+            if (resolver != null)
+            {
+                resolver(this);
+            }
+            return this;
+        }
+
+        /// Promise this Binding to any instance of Type <T>
+        public IInjectionBinding SupplyTo<T>()
+        {
+            return SupplyTo(typeof(T));
+        }
+
+        /// Promise this Binding to any instance of Type type
+        public IInjectionBinding SupplyTo(Type type)
+        {
+            supplyList.Add(type);
+            if (resolver != null)
+            {
+                resolver(this);
+            }
+            return this;
+        }
+
+        /// Remove the promise to supply this binding to Type <T>
+        public IInjectionBinding Unsupply<T>()
+        {
+            return Unsupply(typeof(T));
+        }
+
+        /// Remove the promise to supply this binding to Type type
+        public IInjectionBinding Unsupply(Type type)
+        {
+            supplyList.Remove(type);
+            return this;
+        }
+
+        public object[] GetSupply()
+        {
+            return supplyList.value as object[];
+        }
+
+        public new IInjectionBinding Bind<T>()
+        {
+            return base.Bind<T>() as IInjectionBinding;
+        }
+
+        public new IInjectionBinding Bind(object key)
+        {
+            return base.Bind(key) as IInjectionBinding;
+        }
+
+        public new IInjectionBinding To<T>()
+        {
+            return base.To<T>() as IInjectionBinding;
+        }
+
+        public new IInjectionBinding To(object o)
+        {
+            return base.To(o) as IInjectionBinding;
+        }
+
+        public new IInjectionBinding ToName<T>()
+        {
+            return base.ToName<T>() as IInjectionBinding;
+        }
+
+        public new IInjectionBinding ToName(object o)
+        {
+            return base.ToName(o) as IInjectionBinding;
+        }
+
+        public new IInjectionBinding Named<T>()
+        {
+            return base.Named<T>() as IInjectionBinding;
+        }
+
+        public new IInjectionBinding Named(object o)
+        {
+            return base.Named(o) as IInjectionBinding;
+        }
+    }
+}
